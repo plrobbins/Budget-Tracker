4e332d0ce73ad8128e74014a63503440456f7efc
Philip Robbins <plrobbins68@outlook.com>
2020-11-21 20:45:51 -0500

    updating the server.js

--- server.js
+++ server.js
@@ -17,9 +17,10 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false,
+  useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false
+
 });
 
 // routes
